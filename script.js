79d481d89b1b59d8f49a87e26ac14f0313bb294d
djighoul29 <51969410+djighoul29@users.noreply.github.com>
2023-06-28 21:44:50 -0500

    Кнопки, новий контейнер

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+/*
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
+*/
 
+var color = 'rgba(0,0,0,0.65)';
+var menu_obj = $('.fixed-top');
+var drop_obj = $('#navbarMain');
+var lock = false;
+
+/*
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
 });
@@ -18,4 +26,30 @@ btnPopup.addEventListener('click', ()=> {
 
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
+});
+*/
+
+$(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if(scroll < 64 && lock == false){
+        menu_obj.css('background', 'transparent');
+        drop_obj.css('background', 'transparent');
+    } else if (scroll < 64 && lock == true) {
+        menu_obj.css('background', color);
+        drop_obj.css('background', color);
+    } else {
+        menu_obj.css('background', color);
+    }
+});
+
+$(".navbar-toggler").click(function() {
+    if (lock == false) {
+        menu_obj.css('background', color);
+        drop_obj.css('background', color);
+        lock = true;
+    }else if(lock == true && $(window).scrollTop() < 64) {
+        menu_obj.css('background', 'transparent');
+        drop_obj.css('background', 'transparent');
+        lock = false;
+    }
 });
